90528ba5506f6d31e9b1ec029cadc2d82cba9fe8
ROSTAND LELE <rostoinfocus@gmail.com>
2024-12-23 15:38:47 +0100

    completed
    completed

--- count/src/App.vue
+++ count/src/App.vue
@@ -1,21 +1,44 @@
+<template>
+  <div class="grid gap-4 justify-center items-center">
+    <h1 class="text-7xl text-center text-red-600 font-bold underline">
+      Count
+    </h1>
 
+    <div class="text-center text-6xl" >{{ count }}</div>
+    <div class="s">
+      <button 
+        @click="increment" 
+        class="mt-5 mr-4 px-7 py-4 bg-green-500 rounded-md text-3xl text-white"
+      >
+        Increment
+      </button>
 
- <template>
-  <div class=" grid gap-4 justify-center items-center"> 
-          <h1 class="text-7xl text-red-600 font-bold underline">
-        Count 
-        </h1>
-        <button @click="increment" class="mt-5 px-7 py-4 bg-green-500 rounded-md text-3xl text-white" > increment = {{ count }}</button>
+      <button 
+        @click="decrement"
+        :class="[
+          'mt-5 px-7 py-4 rounded-md text-3xl text-white',
+          count > 0 ? 'bg-green-500' : 'bg-green-100'
+        ]"
+        :disabled="count === 0"
+      >
+        Decrement
+      </button>
+    </div>
   </div>
 </template>
 
+<script setup>
+import { ref } from 'vue'
 
-  <script setup>
-  import { ref } from 'vue'
-  const count = ref(0);
+let count = ref(0);
 
-  const increment = () => {
-    count.value++;
+const increment = () => {
+  count.value++;
+}
+
+const decrement = () => {
+  if (count.value > 0) {
+    count.value--;
   }
-  
-  </script>
+}
+</script>
---
+++ count/src/App0.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class=" grid gap-4 justify-center items-center">
+    <h1 class="text-7xl text-center text-red-600 font-bold underline">
+      Count
+    </h1>
+
+
+    <div class="text-center text-6xl">{{ count }}</div>
+    <div class="s">
+
+      <button @click="increment" class="mt-5 mr-4 px-7 py-4 bg-green-500 rounded-md text-3xl text-white"> increment
+      </button>
+
+      <span class="" v-if="count > 0">
+        <button @click="decrement" class="mt-5 px-7 py-4 bg-green-500 rounded-md text-3xl text-white"> Decrement
+        </button>
+      </span>
+      <span  v-else>
+        <button class="disabled:opacity-0 disabled:cursor-none mt-5 px-7 py-4 bg-green-100 rounded-md text-3xl text-white"> Decrement
+        </button>
+      </span>
+
+    </div>
+   
+  </div>
+
+</template>
+
+
+<script setup>
+import { ref } from 'vue'
+let count = ref(0);
+
+console.log(count);
+
+
+const increment = () => {
+  count.value++;
+}
+const decrement = () => {
+  count.value--;
+}
+
+</script>
